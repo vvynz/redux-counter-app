1ea3537a4a9e4f29a1c60472f91b0b72f3f86564
Vivian Zhang <vvynz12@gmail.com>
2022-08-11 16:31:17 -0400

    Create an increment & decrement function and an increment & decrement button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+
+  const increment = () => {
+
+  }
+
+  const decrement = () => {
+
+  }
+
   return (
     <div>
       <h1>Counter App</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
