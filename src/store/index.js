aae01d3194edb55b67ca1d4aaaad588d7880a9cb
Vivian Zhang <vvynz12@gmail.com>
2022-08-11 16:53:57 -0400

    Create an if statement where if action.type === INC, we return a obj with the state property

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore } from "redux";
 
 const reducerFn = (state = { counter: 0 }, action) => {
+  // should always be a synchronous func
+  // shoutl not mutate the original state
+  // if we do mutate state inside store, it will crash the app
+
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+
   return state;
 };
 
